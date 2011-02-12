6f144c51c197043195dc0702bcfcbf6e13c54de4
James Cranch <cranch@cantab.net>
2011-02-12 01:41:45 +0000

    Better, more general search&replace. Ready for funtimes, I think.

--- formatter/prefixtree.py
+++ formatter/prefixtree.py
@@ -3,7 +3,87 @@ Prefix trees
   http://en.wikipedia.org/wiki/Trie
 
 Functionality is split between PrefixMaster and PrefixTree.
+
+There are two concepts we have invented to make the code general.
+
+1. A "charstream" is an iter of pairs (c,n) where c is a character, and n is an integer. This is supposed to represent characters of a patched string together with their positions in the original.
+   Of course, a charstream can be produced from a string using enumerate.
+
+2. A "normalising function" takes an iter of chars and returns a character stream.
 """
+
+
+from itertools import tee, islice
+
+
+def compose_normalisers(g,f):
+    "Composes two normalising functions"
+    return (lambda l: composed_normalisers(g,f,l))
+
+
+def composed_normalisers(g,f,l):
+    "Composes two normalising functions and applies them to l. The problem, of course, is in renumbering"
+    (fl,fl2) = tee(f(l))
+    gfl = g(x for (i,x) in fl2)
+    c = 0
+    try:
+        (i,x) = fl.next()
+        for (j,y) in gfl:
+            for z in range(j-c):
+                (i,x) = fl.next()
+            c = j
+            yield (i,y)
+    except StopIteration:
+        pass
+
+
+def unenumerate(l):
+    "Recover a string from an enumeration"
+    return "".join(c for (n,c) in l)
+
+
+def limply_normalise(l):
+    "Set all non-alphanumerics to a single space, set everything to lowercase."
+    excited = True
+    for (n,c) in enumerate(l):
+        if excited and not c.isalnum():
+            excited = False
+            yield (n,' ')
+        elif c.isalnum():
+            excited = True
+            yield (n,c.lower())
+            
+
+def violently_normalise(l):
+    "Strip out all non-alphanumerics, set everything to lowercase."
+    for (n,c) in enumerate(l):
+        if c.isalnum():
+            yield (n,c.lower())
+
+
+def remove_html(l):
+    "Strip out tags; replace &amp; and &lt; and &gt;"
+
+    def untag(l):
+        outside = True
+        for (n,c) in enumerate(l):
+            if outside:
+                if c == "<":
+                    outside = False
+                else:
+                    yield (n,c)
+            else:
+                if c == ">":
+                    outside = True
+
+    p = PrefixTree().populate([("&amp;","&"),("&lt;","<"),("&gt;",">")])
+
+    def unentity(l):
+        return p.search_and_replace(enumerate, lambda x,y:y, l)
+
+    return composed_normalisers(unentity, untag, l)
+
+
 
 
 class PrefixMaster:
@@ -26,127 +106,126 @@ class PrefixMaster:
             raise IndexError("No such key")
         return x
 
-    def search(self,l):
+    def search_string(self,s):
         """
         Searches through a string, and yields all maximal matches.
         Yields pairs consisting of the start position and the key.
         """
-        return normalising_search(lambda x:x,l)
+        return self.search(enumerate(s))
 
-    def normalising_search(self,p,l):
+    def search(self,l):
         """
-        Calls p on every character; p returns a string (well,
-        maybe just an iterable of characters). It is this string
-        that is used for the prefix tree. Otherwise like search.
+        Searches through a charstream, and yields all maximal matches.
+        Yields pairs consisting of the start position and the key.
         """
         matches = []
-        for (k,c1) in enumerate(l):
-            for c in p(c1):
-                matches.append((self,k,None))
-                newmatches = []
-                for (n,i,x) in matches:
-                    n2 = n.child(c)
-                    x2 = n.content() or x
-                    if n2 is None:
-                        if x2 is not None:
-                            yield (i,x2)
-                    else:
-                        newmatches.append((n2,i,x2))
-                matches = newmatches
+        for (k,c) in l:
+            matches.append((self,k,None))
+            newmatches = []
+            for (n,i,x) in matches:
+                n2 = n.child(c)
+                x2 = n.content() or x
+                if n2 is None:
+                    if x2 is not None:
+                        yield (i,x2)
+                else:
+                    newmatches.append((n2,i,x2))
+            matches = newmatches
 
     def search_and_replace(self,p,f,l):
         """
-        Yields successive characters of the input, appropriately normalised,
-        with matches replaced by f of the matching input and the key.
+        Yields successive characters of the input string, normalised by
+        normalising function p, with matches replaced by f of the matching
+        input and the key.
 
-        As we go through, we maintain:
-         - a list of input characters we're not sure about
-         - the start position in that document of those characters
+        As we go along, we maintain:
+         - the number of input characters we've processed
          - a set of possible matches, which are tuples consisting of:
             - current prefixtree node (or None, if no further match is possible)
             - start index in input string
             - last match end index in input string (None, if no match made yet)
             - last match value (None, if no match made yet)
         """
-        unprocessed = []
-        shift = 0
-        matches = []
 
-        def advance_match_states(c,k,l):
-            newl = []
-            for (node,start,lastend,lastval) in matches:
+        (inchars1,inchars2) = tee(iter(l))
+        inchars = enumerate(inchars1)
+        normalised = p(inchars2)
+        
+        count = 0
+        states = []
+
+        def advance_states(c,k,states):
+            newstates = []
+            for (node,start,lastend,lastval) in states:
                 node2 = node.child(c)
                 end2 = (node.content() and k) or lastend
                 val2 = node.content() or lastval
                 if node2 or val2:
-                    newl.append((node2,start,end2,val2))
-            return newl
+                    newstates.append((node2,start,end2,val2))
+            return newstates
 
-        def finalise_match_states(k,l):
-            newl = []
-            for (node,start,lastend,lastval) in matches:
+        def finalise_states(k,states):
+            newstates = []
+            for (node,start,lastend,lastval) in states:
                 end2 = (node.content() and k) or lastend
                 val2 = node.content() or lastval
                 if val2:
-                    newl.append((node,start,end2,val2))
-            return newl
+                    newstates.append((node,start,end2,val2))
+            return newstates
 
-        for (k,c1) in enumerate(l):
-            unprocessed.append(c1)
-            for c in p(c1):
-                matches.append((self,k,None,None))
-                matches = advance_match_states(c,k,matches)
+        for (k,c) in normalised:
+            states.append((self,k,None,None))
+            states = advance_states(c,k,states)
 
-                # process matches
-                while len(matches)>0 and matches[0][0] is None and matches[0][2] is not None:
-                    # release replacement
-                    (node,start,end,val) = matches[0]
-                    matchstr = "".join(unprocessed[(start-shift):(end-shift)])
-                    for x in f(matchstr,val):
-                        yield x
-                    matches = matches[1:]
+            # process matches
+            while len(states)>0 and states[0][0] is None and states[0][2] is not None:
+                # release a replacement
+                (node,start,end,val) = states[0]
+                for x in f(unenumerate(islice(inchars,end-start)),val):
+                    yield (start,x)
+                count = end
+                states = states[1:]
 
-                    # cancel work on matches which clash
-                    while len(matches)>0 and matches[0][1]<end:
-                        matches = matches[1:]
-
-                    # flush matched text
-                    unprocessed = unprocessed[(end-shift):]
-                    shift = end
+                # cancel work on matches which clash
+                while len(states)>0 and states[0][1]<end:
+                    states = states[1:]
 
             # release unmatchable data as unchanged
-            if len(matches)>0:
-                newshift=matches[0][1]
+            if len(states)>0:
+                newcount = states[0][1]
             else:
-                newshift=k
-            if newshift>shift:
-                for x in unprocessed[:(newshift-shift)]:
-                    yield x
-                unprocessed = unprocessed[(newshift-shift):]
-                shift=newshift
+                newcount = k
+            if newcount>count:
+                for (k2,c2) in islice(inchars,newcount-count):
+                    yield (k2,c2)
+                count=newcount
 
-        # deal with what's left: go through remaining matches
-        matches = finalise_match_states(k+1,matches)
+        # deal with what's left over
+        states = finalise_states(k+1,states)
 
-        while len(matches)>0:
+        while len(states)>0:
             # release replacement
-            (node,start,end,val) = matches[0]
-            matchstr = "".join(unprocessed[(start-shift):(end-shift)])
-            for x in f(matchstr,val):
-                yield x
-            matches = matches[1:]
+            (node,start,end,val) = states[0]
+            for x in f(unenumerate(islice(inchars,start-count,end-count)),val):
+                yield (start,x)
+            count = end
+            states = states[1:]
 
-            # cancel work on matches which clash
-            while len(matches)>0 and matches[0][1]<end:
-                matches = matches[1:]
+            # cancel clashes
+            while len(states)>0 and states[0][1]<end:
+                states = states[1:]
 
-            # flush matched text
-            unprocessed = unprocessed[(end-shift):]
-            shift = end
+            # release free text
+            if len(states)>0:
+                newcount = states[0][1]
+                if newcount>count:
+                    for (k2,c2) in islice(inchars,newcount-count):
+                        yield (k2,c2)
+                    count=newcount
 
-        # release unmatchable data as unchanged
-        for x in unprocessed:
-            yield x
+        # release all remaining text
+        for (k2,c2) in inchars:
+            yield (k2,c2)
 
 
 
--- formatter/test_prefixtree.py
+++ formatter/test_prefixtree.py
@@ -1,14 +1,38 @@
 from prefixtree import *
 
 
-def normalise(c):
-    return c.lower()
-
 def handle(n,s):
     return "{%s:%s}"%(n,s)
 
 p = PrefixTree().populate([("badger","grrrr"),("cat","miaow"),("dog","woof"),("dogfish","woof&glug"),("fish","glug")])
 
-s = "".join(p.search_and_replace(normalise,handle,"Dog, dogfish, badger, badgerfish, cat, catfish, another dog"))
+print unenumerate(p.search_and_replace(limply_normalise,handle,"Dog, dogfish, badger, badgerfish, cat, catfish, and that'll do"))
 
-print s
+
+
+print
+
+
+
+print unenumerate(remove_html('<p>This is a paragraph containing a <a href="link.html">link</a> and some <strong>strongly worded</strong> parts, but I hope &amp; pray that the formatting won\'t survive, except for some &lt;relics&gt;.</p>'))
+
+
+
+print
+
+
+
+def taggify(n,_):
+    return "<em>%s</em>"%n
+
+p = PrefixTree().populate(sorted([("Flopsy",True),("Mopsy",True),("Cotton-tail",True),("Peter",True)]))
+
+s = """<p>Once upon a time there were four little Rabbits, &amp; their names were &mdash;</p>
+<ol>
+  <li>Flopsy,</li>
+  <li>Mopsy,</li>
+  <li>Cotton-tail, and</li>
+  <li>Peter.</li>
+</ol>"""
+
+print unenumerate(p.search_and_replace(remove_html,taggify,s))
