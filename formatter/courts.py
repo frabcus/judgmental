51034f3a70218dfa81e61417e188df5f691b3453
Nick Bull <nicholaspbull@gmail.com>
2011-02-27 19:53:36 +0000

    Split out list of courts into separate file.

---
+++ formatter/courts.py
@@ -0,0 +1,73 @@
+"""
+Reads file metadata from database and transforms files.
+"""
+
+# List of long and short names for courts.  The short names mostly match those
+# in neutral citations.
+
+# This could be better placed somewhere else, possibly in the courts table of
+# judgmental.db, if that could be stable instead of regenerated.
+
+courts = [
+("UKSC", "United Kingdom Supreme Court"),
+("NICA", "Court of Appeal in Northern Ireland Decisions"),
+("CAT", "United Kingdom Competition Appeals Tribunal"),
+("UKPC", "Privy Council Decisions"),
+("UKVAT", "United Kingdom VAT & Duties Tribunals Decisions"),
+("UKVAT-Customs", "United Kingdom VAT & Duties Tribunals (Customs) Decisions"),
+("UKVAT-Excise", "United Kingdom VAT & Duties Tribunals (Excise) Decisions"),
+("UKVAT-Landfill", "United Kingdom VAT & Duties Tribunals (Landfill Tax) Decisions"),
+("UKVAT-IPT", "United Kingdom VAT & Duties Tribunals (Insurance Premium Tax) Decisions"),
+("UKEAT", "United Kingdom Employment Appeal Tribunal"),
+("UKUT-AAC", "Upper Tribunal (Administrative Appeals Chamber)"),
+("UKUT-LC", "United Kingdom Upper Tribunal (Lands Chamber)"),
+("UKUT-TCC-", "United Kingdom Upper Tribunal (Finance and Tax)"),
+("DRS", "Nominet UK Dispute Resolution Service"),
+("UKSIAC", "Special Immigrations Appeals Commission"),
+("ScotHC", "Scottish High Court of Justiciary Decisons"),
+("ECHR", "European Court of Human Rights"),
+("UKSPC", "United Kingdom Special Commissioners of Income Tax Decisions"),
+("ScotCS", "Scottish Court of Session Decisions"),
+("IECCA", "Irish Court of Criminal Appeal"),
+("UKHL", "United Kingdom House of Lords Decisions"),
+("IEHC", "High Court of Ireland Decisions"),
+("NIFET", "Fair Employment Tribunal Northern Ireland Decisions"),
+("NISSCSC", "Northern Ireland - Social Security and Child Support Commissioners' Decisions"),
+("EUECJ", "Court of Justice of the European Communities (including Court of First Instance Decisions)"),
+("EWCST", "England and Wales Care Standards Tribunal"),
+("IECA", "Irish Competition Authority Decisions"),
+("IECA-Notice", "Irish Competition Authority Decisions (Notice Division)"),
+("EWCA-Civ", "England and Wales Court of Appeal (Civil Division) Decisions"),
+("EWCA-Crim", "England and Wales Court of Appeal (Criminal Division) Decisions"),
+("IEIC", "Irish Information Commissioner's Decisions"),
+("IESC", "Supreme Court of Ireland Decisions"),
+("IEDPC", "Irish Data Protection Commission Case Studies"),
+("EWCC-Fam", "England and Wales County Court (Family)"),
+("NIQB", "High Court of Justice in Northern Ireland Queen's Bench Division Decisions"),
+("NIFam", "High Court of Justice in Northern Ireland Family Division Decisions"),
+("NIMaster", "Northand Ireland High Court of Justice, Master's decisions"),
+("NICH", "High Court of Justice in Northern Ireland Chancery Division Decisions"),
+("EWLands", "England and Wales Lands Tribunal"),
+("EWPCC", "England and Wales Patents County Court"),
+("UKIT", "United Kingdom Information Tribunal including the National Security Appeals Panel"),
+("UKFSM", "United Kingdom Financial Services and Markets Tribunals Decisions"),
+("ScotSC", "Scottish Sheriff Court Decisions"),
+("NIIT", "Industrial Tribunals Northern Ireland Decisions"),
+("EWHC-Comm", "England and Wales High Court (Commercial Court) Decisions"),
+("EWHC-QB", "England and Wales High Court (Queen's Bench Division) Decisions"),
+("EWHC-Admin", "England and Wales High Court (Administrative Court) Decisions"),
+("EWHC-Ch", "England and Wales High Court (Chancery Division) Decisions"),
+("EWHC-TCC", "England and Wales High Court (Technology and Construction Court) Decisions"),
+("EWHC-Pat", "England and Wales High Court (Patents Court) Decisions"),
+("EWHC-Fam", "England and Wales High Court (Family Division) Decisions"),
+("EWHC-Admlty", "England and Wales High Court (Admiralty Division) Decisions"),
+("EWHC-Costs", "England and Wales High Court (Senior Courts Costs Office) Decisions"),
+("EWHC-KB", "England and Wales High Court (King's Bench Division) Decisions"),
+("EWHC-Exch", "England and Wales High Court (Exchequer Court) Decisions"),
+("EWHC-Mercantile", "Mercantile Court"),
+("UKIAT", "United Kingdom Asylum and Immigration Tribunal"),
+("NICC", "Crown Court for Northern Ireland Decisions"),
+("UKSSCSC", "UK Social Security and Child Support Commissioners' Decisions"),
+("UKFTT-TC", "United Kingdom First Tier Tribunal (Tax)"),
+("UKFTT-HESC", "First-tier Tribunal (Health Education and Social Care Chamber)")]
+
--- formatter/indexes.py
+++ formatter/indexes.py
@@ -11,81 +11,13 @@ import traceback
 
 from StringIO import StringIO
 
+import courts
 from general import *
-
 
 # must use a global variable to ensure it is visible throughout the process pool
 with open("template_index.html",'r') as html_template_file:
     html_template_index_stringio = StringIO(html_template_file.read())
 
-# List of long and short names for courts.  The short names mostly match those
-# in neutral citations.
-
-# This could be better placed somewhere else, possibly in the courts table of
-# judgmental.db, if that could be stable instead of regenerated.
-
-courts = [
-("UKSC", "United Kingdom Supreme Court"),
-("NICA", "Court of Appeal in Northern Ireland Decisions"),
-("CAT", "United Kingdom Competition Appeals Tribunal"),
-("UKPC", "Privy Council Decisions"),
-("UKVAT", "United Kingdom VAT & Duties Tribunals Decisions"),
-("UKVAT(Customs)", "United Kingdom VAT & Duties Tribunals (Customs) Decisions"),
-("UKVAT(Excise)", "United Kingdom VAT & Duties Tribunals (Excise) Decisions"),
-("UKVAT(Landfill)", "United Kingdom VAT & Duties Tribunals (Landfill Tax) Decisions"),
-("UKVAT(IPT)", "United Kingdom VAT & Duties Tribunals (Insurance Premium Tax) Decisions"),
-("UKEAT", "United Kingdom Employment Appeal Tribunal"),
-("UKUT(AAC)", "Upper Tribunal (Administrative Appeals Chamber)"),
-("UKUT(LC)", "United Kingdom Upper Tribunal (Lands Chamber)"),
-("UKUT(TCC)", "United Kingdom Upper Tribunal (Finance and Tax)"),
-("DRS", "Nominet UK Dispute Resolution Service"),
-("UKSIAC", "Special Immigrations Appeals Commission"),
-("ScotHC", "Scottish High Court of Justiciary Decisons"),
-("ECHR", "European Court of Human Rights"),
-("UKSPC", "United Kingdom Special Commissioners of Income Tax Decisions"),
-("ScotCS", "Scottish Court of Session Decisions"),
-("IECCA", "Irish Court of Criminal Appeal"),
-("UKHL", "United Kingdom House of Lords Decisions"),
-("IEHC", "High Court of Ireland Decisions"),
-("NIFET", "Fair Employment Tribunal Northern Ireland Decisions"),
-("NISSCSC", "Northern Ireland - Social Security and Child Support Commissioners' Decisions"),
-("EUECJ", "Court of Justice of the European Communities (including Court of First Instance Decisions)"),
-("EWCST", "England and Wales Care Standards Tribunal"),
-("IECA", "Irish Competition Authority Decisions"),
-("IECA (Notice)", "Irish Competition Authority Decisions (Notice Division)"),
-("EWCA Civ", "England and Wales Court of Appeal (Civil Division) Decisions"),
-("EWCA Crim", "England and Wales Court of Appeal (Criminal Division) Decisions"),
-("IEIC", "Irish Information Commissioner's Decisions"),
-("IESC", "Supreme Court of Ireland Decisions"),
-("IEDPC", "Irish Data Protection Commission Case Studies"),
-("EWCC (Fam)", "England and Wales County Court (Family)"),
-("NIQB", "High Court of Justice in Northern Ireland Queen's Bench Division Decisions"),
-("NIFam", "High Court of Justice in Northern Ireland Family Division Decisions"),
-("NIMaster", "Northand Ireland High Court of Justice, Master's decisions"),
-("NICH", "High Court of Justice in Northern Ireland Chancery Division Decisions"),
-("EWLands", "England and Wales Lands Tribunal"),
-("EWPCC", "England and Wales Patents County Court"),
-("UKIT", "United Kingdom Information Tribunal including the National Security Appeals Panel"),
-("UKFSM", "United Kingdom Financial Services and Markets Tribunals Decisions"),
-("ScotSC", "Scottish Sheriff Court Decisions"),
-("NIIT", "Industrial Tribunals Northern Ireland Decisions"),
-("EWHC (Comm)", "England and Wales High Court (Commercial Court) Decisions"),
-("EWHC (QB)", "England and Wales High Court (Queen's Bench Division) Decisions"),
-("EWHC (Admin)", "England and Wales High Court (Administrative Court) Decisions"),
-("EWHC (Ch)", "England and Wales High Court (Chancery Division) Decisions"),
-("EWHC (TCC)", "England and Wales High Court (Technology and Construction Court) Decisions"),
-("EWHC (Pat)", "England and Wales High Court (Patents Court) Decisions"),
-("EWHC (Fam)", "England and Wales High Court (Family Division) Decisions"),
-("EWHC (Admlty)", "England and Wales High Court (Admiralty Division) Decisions"),
-("EWHC (Costs)", "England and Wales High Court (Senior Courts Costs Office) Decisions"),
-("EWHC KB", "England and Wales High Court (King's Bench Division) Decisions"),
-("EWHC Exch", "England and Wales High Court (Exchequer Court) Decisions"),
-("EWHC (Mercantile)", "Mercantile Court"),
-("UKIAT", "United Kingdom Asylum and Immigration Tribunal"),
-("NICC", "Crown Court for Northern Ireland Decisions"),
-("UKSSCSC", "UK Social Security and Child Support Commissioners' Decisions"),
-("UKFTT (TC)", "United Kingdom First Tier Tribunal (Tax)"),
-("UKFTT (HESC)", "First-tier Tribunal (Health Education and Social Care Chamber)")]
 
 months = ["", "January", "February", "March", "April", "May", "June",
 	"July", "August", "September", "October", "November", "December"]
@@ -122,7 +54,7 @@ def make_indexes(dbfile_name, logfile, output_dir, use_multiprocessing):
             courts = list(cursor.execute('SELECT courtid, name FROM courts'))
     	
     	process_pool.apply_async(make_top_index,(output_dir,),callback=convert_report("top"))
-    	for (id, name) in courts:
+    	for (id, name) in courts.courts:
     		process_pool.apply_async(make_court_index,(id,name,dbfile_name,use_multiprocessing,output_dir),callback=convert_report(name))
 
     broadcast(logfile,"Converted %d files successfully"%finished_count.count)
