ae67412925f63610a661916d5d53cfadbcb0c6e4
Chris <no@spam>
2011-03-13 20:37:09 +0000

    No longer require citations in citation table to be non-unique.
    This will break some things...

--- formatter/analyse.py
+++ formatter/analyse.py
@@ -50,7 +50,7 @@ def analyse(file_list, dbfile_name, logfile, use_multiprocessing):
 def create_tables(cursor):
     "Create tables in an SQL database"
     s = ['CREATE TABLE courts (courtid INTEGER PRIMARY KEY ASC, name TEXT UNIQUE)',
-         'CREATE TABLE citations (citationid INTEGER PRIMARY KEY ASC, citation TEXT UNIQUE, judgmentid INTEGER)',
+         'CREATE TABLE citations (citationid INTEGER PRIMARY KEY ASC, citation TEXT, judgmentid INTEGER)',
          'CREATE TABLE judgments (judgmentid INTEGER PRIMARY KEY ASC, title TEXT, date DATE, courtid INTEGER, filename TEXT UNIQUE, bailii_url TEXT UNIQUE, judgmental_url TEXT UNIQUE)',
          'CREATE TABLE parties (partyid INTEGER PRIMARY KEY ASC, name TEXT, position INTEGER, judgmentid INTEGER)']
     create_tables_interactively(cursor,['courts','citations','judgments','parties'],s)
