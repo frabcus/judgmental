1ee604a9ef6e3c67dc618df5719673623d222416
Chris <no@spam>
2011-05-20 13:12:49 +0100

    update database structure in crossreferencing stage

--- formatter/crossreference.py
+++ formatter/crossreference.py
@@ -24,10 +24,10 @@ def crossreference(file_list, dbfile_name, logfile, use_multiprocessing):
         create_table(cursor)
 
         print "Making prefix tree"
-        cursor.execute('SELECT DISTINCT citationcode,citationid FROM citations ORDER BY citationcode')
+        cursor.execute('SELECT DISTINCT citationcode,citationcodeid FROM citationcodes ORDER BY citationcode')
         sorted_citations = [(a,i) for (a,i) in cursor]
         citationtree.populate(sorted_citations)
-        broadcast(logfile,"Read %d citation formats in database"%len(sorted_citations))
+        broadcast(logfile,"Read %d citation codes from database"%len(sorted_citations))
 
         def crossreference_report(basename):
             "Callback function; reports on success or failure"
@@ -56,7 +56,7 @@ def crossreference(file_list, dbfile_name, logfile, use_multiprocessing):
         cursor.execute('SELECT count() FROM crossreferences')
         crossreference_count = cursor.fetchone()[0]
         broadcast(logfile,"Found %d crossreferences (including selfreferences)"%crossreference_count)
-        cursor.execute('DELETE FROM crossreferences WHERE crossreferenceid IN (SELECT crossreferenceid FROM crossreferences JOIN citations ON crossreferences.citationid = citations.citationid WHERE crossreferences.judgmentid = citations.judgmentid)')
+        cursor.execute('DELETE FROM crossreferences WHERE crossreferenceid IN (SELECT crossreferenceid FROM crossreferences JOIN judgmentcodes ON crossreferences.citationcodeid = judgmentcodes.citationcodeid WHERE crossreferences.judgmentid = judgmentcodes.judgmentid)')
         cursor.execute('SELECT count() FROM crossreferences')
         crossreference_count = cursor.fetchone()[0]
         broadcast(logfile,"Found %d crossreferences (after removing selfreferences)"%crossreference_count)
@@ -64,7 +64,7 @@ def crossreference(file_list, dbfile_name, logfile, use_multiprocessing):
 
 
 def create_table(cursor):
-    create_tables_interactively(cursor,['crossreferences'],['CREATE TABLE crossreferences (crossreferenceid INTEGER PRIMARY KEY ASC, judgmentid INTEGER, citationid INTEGER)'])
+    create_tables_interactively(cursor,['crossreferences'],['CREATE TABLE crossreferences (crossreferenceid INTEGER PRIMARY KEY ASC, judgmentid INTEGER, citationcodeid INTEGER)'])
 
 
 def crossreference_file(fullname,basename,dbfile_name,use_multiprocessing):
@@ -87,8 +87,8 @@ def write_crossreferences_to_sql(citationset,basename,cursor):
             judgmentid = jids[0][0]
         else:
             raise NoMetadata
-        for citationid in citationset:
-            cursor.execute('INSERT INTO crossreferences(judgmentid,citationid) VALUES (?,?)', (judgmentid,citationid))
+        for citationcodeid in citationset:
+            cursor.execute('INSERT INTO crossreferences(judgmentid,citationcodeid) VALUES (?,?)', (judgmentid,citationcodeid))
     except sqlite.IntegrityError, e:
         raise SqliteIntegrityError(e)
 
