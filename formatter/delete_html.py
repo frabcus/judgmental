db9a10c0c66b5f6099391adc5563b5036a0c0394
Chris <no@spam>
2011-06-20 18:46:35 +0100

    Fix delete_html.py bugs

--- formatter/delete_html.py
+++ formatter/delete_html.py
@@ -2,10 +2,10 @@ import os
 
 def delete_html(conversion_start,output_dir):
     i = 0
-    for (path,dirs,files) in os.path.walk(output_dir):
+    for (path,dirs,files) in os.walk(output_dir):
         for basename in files:
             fullname = os.path.join(path,basename)
-            if f[-5:]==".html" and os.path.getmtime(fullname) < conversion_start:
+            if fullname.endswith(".html") and os.path.getmtime(fullname) < conversion_start:
                 i += 1
                 print "delete_html: %d. %s"%(i,basename)
                 os.remove(fullname)
