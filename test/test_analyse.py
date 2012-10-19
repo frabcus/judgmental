ae115880eae95d9597d4c596bda3fb792c6a8479
Ryan Jendoubi <ryan.jendoubi@gmail.com>
2012-10-19 20:27:28 +0100

    Make test_analyse.py easier to expand incrementally

--- test/test_analyse.py
+++ test/test_analyse.py
@@ -7,28 +7,51 @@ import re
 from cStringIO import StringIO
 from lxml import html
 
-from judgmental.analyse import analyse_file
+from judgmental.analyse import find_date
+from judgmental.general import open_bailii_html
 
 class TestDateParsing(unittest.TestCase):
 
     def setUp(self):
-        # Files and the correct metadata for each
+        # Files and the correct metadata for each.
+        # For each file, tests will only be performed for those metadata
+        # with which we are concerned.
         self.test_subjects = \
-            {'_scot_cases_ScotHC_2001_62.html': \
-                { 'date': '2001-07-25' } \
+            {'date': \
+                { '_scot_cases_ScotHC_2001_62.html': '2001-07-25' } \
             }
 
-        ### Change location of input files here!
+        # Will be populated by a (page, titletag, title) (value) for
+        # each filename (key) as the tests run.
+        self.inputs_cache = {}
+
+        # Location of input files
         this_dir = os.path.abspath(\
             os.path.realpath(os.path.dirname(__file__)))
         self.input_dir = os.path.join(this_dir, 'input');
 
+    def _get_inputs(self, filename):
+        if not filename in self.inputs_cache.keys():
+            self.inputs_cache[filename] = ()
+
+            page = html.parse(\
+                open_bailii_html(os.path.join(self.input_dir, filename))\
+            )
+            titletag = page.find("//title")
+            title = re.sub('  +', ' ',\
+                page.find("//title").text.replace('\n', ' '))
+
+            self.inputs_cache[filename] = (page, titletag, title)
+
+        return self.inputs_cache[filename]
+            
+
     def test_date_parsing(self):
-        for k in self.test_subjects:
-            (s, metadata) = analyse_file(os.path.join(self.input_dir, k))
-            self.assertTrue(s) # Analysis apparently successful
-            self.assertEqual(metadata['date'].strftime('%Y-%m-%d'),\
-                self.test_subjects[k]['date'])
+        for f in self.test_subjects['date']:
+            page, titletag, title = self._get_inputs(f) 
+            found_date = find_date(page, titletag, title)
+            self.assertEqual(found_date.strftime('%Y-%m-%d'),\
+                self.test_subjects['date'][f])
 
 if __name__ == '__main__':
     unittest.main()
