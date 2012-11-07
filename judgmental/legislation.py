490f820744292c8f959c0ba0f10b7a6618b8b82b
Ryan Jendoubi <ryan.jendoubi@gmail.com>
2012-11-07 02:47:48 +0000

    pylinting legislation.py

--- judgmental/legislation.py
+++ judgmental/legislation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 # Obtain list of all legislation from legislation.gov.uk's Atom feed.
 
@@ -8,57 +8,87 @@
 # TODO: Welsh regulations have both English and Welsh names contained
 # in the title attribute.  Currently we just dump all this into the
 # database - would be better to split it up and create two entries.
-		
-# TODO: Old acts sometimes contain "(repealed)" in the title.	
+        
+# TODO: Old acts sometimes contain "(repealed)" in the title.    
 
 import feedparser
 import sys
-from general import *
+from general import \
+    create_tables_interactively,\
+    DatabaseManager
 
-verbose = False
+class ImportLegislation:
 
-def getandparse(uri, cursor):
+    def __init__(self, \
+        dbfile="../../judgmental_nonlive.db", \
+        uritemplate="http://legislation.data.gov.uk/ukpga/%d/data.feed", \
+        verbose=False):
 
-	d = feedparser.parse(uri)
-	if verbose: print d.feed.links
-		
-	for entry in d.entries:
-		title = entry.title
-		link = [l.href for l in entry.links if l.rel=="self"][0]
-		
-		
-		if title[:4] == "The ":
-			title = title[4:]
-			
-		if verbose: print title
-		cursor.execute('INSERT INTO legislation(title,link) VALUES (?,?)',(title,link))
+        self.uritemplate = uritemplate
+        self.dbfile = dbfile
+        self.verbose = verbose
 
-	try:
-		newuri =  [l.href for l in d.feed.links if l.rel=="next"][0]
-		if verbose: print newuri
-		# Strange time loop in 1976
-		if newuri == uri:
-			return None
-		return newuri
-	except IndexError:
-		return None
+    def getandparse(self, uri, cursor):
+        """
+        Grab legislation.gov.uk atom feed, parse, and insert legislation
+        titles and links into db; assumes table `legislation` exists.
+        """
+    
+        d = feedparser.parse(uri)
+        print d
+        quit()
+    
+        if self.verbose: print d.feed.links
+            
+        for entry in d.entries:
+            title = entry.title
+            link = [l.href for l in entry.links if l.rel == "self"][0]
+            
+            
+            if title[:4] == "The ":
+                title = title[4:]
+                
+            if self.verbose: print title
+    
+            cursor.execute('INSERT INTO legislation(title,link) VALUES (?,?)', \
+                (title,link))
+    
+        try:
+            newuri =  [l.href for l in d.feed.links if l.rel == "next"][0]
+            if self.verbose: print newuri
+            # Strange time loop in 1976
+            if newuri == uri:
+                return None
+            return newuri
+        except IndexError:
+            return None
+    
+    def run(self):
+        """
+        Driver method; marshal pre-requisites and call getandparse() for each
+        year 1801 - <this_year>
+        """
+        
+        with DatabaseManager(self.dbfile, False) as cursor:
+            create_tables_interactively(\
+                cursor,\
+                ['legislation'],\
+                ['CREATE TABLE legislation (legislationid INTEGER PRIMARY KEY ASC, title TEXT, link TEXT)']\
+            )
+        
+            for year in range(1801, 2012):
+                if self.verbose: print year
+                uri = self.uritemplate % year
+                while uri is not None:
+                    uri = self.getandparse(uri, cursor)
 
-
-arguments = sys.argv[1:]
-while len(arguments)>0:
-    a = arguments[0]
-    arguments = arguments[1:]
-    if a == "--verbose":
-    	verbose = True
-
-uritemplate = "http://legislation.data.gov.uk/ukpga/%d/data.feed"
-dbfile = "../../judgmental_nonlive.db"
-
-with DatabaseManager(dbfile, False) as cursor:
-	create_tables_interactively(cursor,['legislation'],['CREATE TABLE legislation (legislationid INTEGER PRIMARY KEY ASC, title TEXT, link TEXT)'])
-
-	for year in range(1801,2011):
-		if verbose: print year
-		uri = uritemplate % year
-		while uri is not None:
-			uri = getandparse(uri, cursor)
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    while len(arguments) > 0:
+        a = arguments[0]
+        arguments = arguments[1:]
+        if a == "--verbose":
+            verbose = True
+        
+    importer = ImportLegislation(verbose=verbose)
+    importer.run()
